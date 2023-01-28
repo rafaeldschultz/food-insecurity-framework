995ff82340d0af2af15435cb94de35535d121a12
Rafael Schultz <rafaeldschultz@gmail.com>
2023-01-28 16:14:48 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-setup()
+setup(
+    name='foodinsecurityframework',
+    version='0.0.1',
+    packages=find_packages(),
+)
